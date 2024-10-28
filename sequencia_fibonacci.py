53e64453331616a6546acedd6d26d16d6786cfbc
Fabiocunhac <fabiocunha.c@hotmail.com>
2024-10-28 17:42:56 -0300

    Update sequencia_fibonacci.py

--- sequencia_fibonacci.py
+++ sequencia_fibonacci.py
@@ -9,14 +9,21 @@ def pertence_a_fibonacci(n):
     return b == n
 
 def main():
-    try:
-        numero = int(input("Digite um número para verificar se pertence à sequência de Fibonacci:"))
-        if pertence_a_fibonacci(numero):
-            print(f"O número {numero} pertence à sequência de Fibonacci.")
-        else:
-            print(f"O número {numero} não pertence à sequência de Fibonacci.")
-    except ValueError:
-        print("Por favor, insira um número inteiro válido.")
+    while True:
+        entrada = input("Digite um número para verificar se pertence à sequência de Fibonacci (ou 'Sair' para encerrar): ")
+        
+        if entrada.lower() == "sair":
+            print("Encerrando o programa...")
+            break
+
+        try:
+            numero = int(entrada)
+            if pertence_a_fibonacci(numero):
+                print(f"O número {numero} pertence à sequência de Fibonacci.")
+            else:
+                print(f"O número {numero} não pertence à sequência de Fibonacci.")
+        except ValueError:
+            print("Por favor, insira um número inteiro válido ou 'Sair' para encerrar.")
 
 if __name__ == "__main__":
     main()
